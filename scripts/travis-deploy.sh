52bcfa2fc628f49303faff0bbf6cdea83eb579ee
Buddhika Semasinghe <buddhika.semasinghe@gmail.com>
2019-08-26 23:49:11 +1000

    Corrected Travis yml settings

--- scripts/travis-deploy.sh
+++ scripts/travis-deploy.sh
@@ -1,3 +1,3 @@
 echo "Deploying from Travis CI to S3"
 
-docker run $(printenv | grep -E '^AWS' | sed 's/AWS_/-e /g' | sed 's/-e /-e AWS_/g') fluffy-react-deploy
+docker run $(printenv | grep -E '^AWS' | sed 's/AWS_/-e /g' | sed 's/-e /-e AWS_/g') fluffy-react-aws
